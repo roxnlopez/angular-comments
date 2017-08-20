2cfeaa7bbc3a16cf105f9e9a0c44408b96261503
roxnlopez <roxnlopez@gmail.com>
2017-08-20 17:36:16 -0600

    added restufl routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+
+class quote {
+	update: false;
+	message: string;
+	author: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,14 +14,40 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+	updateMessage;
+	updateAuthor;
+	newQuote: quote;
+
+	constructor() { 
+		this.newQuote = new quote();
+	}
+
   comments = [
-  	{message:'first comment!', author: 'you'},
-    {message: 'nice work!', author: 'your mom'},
-    {message: 'I would also like to congratulate you!', author: 'your dad'}
+  	{update:false, message:'first comment!', author: 'you'},
+    {update:false, message: 'nice work!', author: 'your mom'},
+    {update:false, message: 'I would also like to congratulate you!', author: 'Marley'}
   ];
 
-  constructor() { }
+  onSubmit() {
+  	if(this.newQuote) {
+  		var entry = {
+  			'update': false,
+  			'message': this.newQuote.message,
+  			'author': this.newQuote.author
+  		};
+  	this.comments.push(entry);	
+  	}
+  }
 
+  onDelete(i) {
+  	this.comments.splice(i,1);
+  }
+
+  onUpdate(i) {
+  	this.comments[i].update = false;
+  	this.comments[i].message = this.updateMessage;
+  	this.comments[i].author = this.updateAuthor;
+  }
 
 }
 
